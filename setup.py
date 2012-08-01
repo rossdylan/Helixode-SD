09f6ca5e71defc9781d31cf668c486cb00fc6df8
Ross Delinger <rossdylan@csh.rit.edu>
2012-08-01 12:33:11 -0400

    forgot to add a url to the setup.py file

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
         description="Simple Service discovering library",
         author="Ross Delinger",
         author_email="rossdylan@csh.rit.edu",
+        url='https://github.com/rossdylan/Helixode-SD',
         packages=['hxsd'],
         install_requires=requires,
         entry_points="""
